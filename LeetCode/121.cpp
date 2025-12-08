c628063609e9a065d5d9fb9dc090dbd56fccf1e9
MaraMincho <seemaster300@naver.com>
2025-12-08 13:49:16 +0900

    ps commit

--- LeetCode/121.cpp
+++ LeetCode/121.cpp
@@ -6,9 +6,8 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
         vector<int> d = {};
-        int answer = INTMAX_MAX;
+        int answer = -100000;
         for (int i = 0; i < prices.size(); i ++) {
             auto price = prices[i];
             if (d.empty()) {
@@ -16,12 +15,30 @@ public:
                 continue;
             }
             if (d.front() < price) {
-                answer = answer < price - d.front() ? answer : price - d.front();
+                answer = answer < price - d.front() ? price - d.front(): answer;
             }
             while (!d.empty() && price < d.back()) {
                 d.pop_back();
             }
             d.push_back(price);
         }
+        return answer < 0 ? 0 : answer;
+    }
+
+    int sol2(vector<int>& prices) {
+        int currentMin = prices[0];
+        auto answer = 0;
+
+        for (int index = 1; index < prices.size() ; index ++) {
+            auto price = prices[index];
+            
+            if (answer < price - currentMin) {
+                answer = price - currentMin;
+            }
+            if (price < currentMin) {
+                currentMin = price;
+            }
+        }
+        return answer;
     }
 };
