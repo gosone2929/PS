a00280bb5091e686707b897052c0a00bcff2b7e3
MaraMincho <seemaster300@naver.com>
2025-12-11 08:38:24 +0900

    ps

--- LeetCode/015.cpp
+++ LeetCode/015.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    // Original 
+    vector<vector<int>> threeSum1(vector<int>& nums) {
         vector<vector<int>> answer;
 
         unordered_map<int, int> ts = {};
@@ -54,6 +55,38 @@ public:
         }
         
         return answer;
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans = {};
+        int numSize = nums.size();
+        for (int i = 0; i < numSize - 1; i += 1) {
+            if (i > 0 and nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            auto iValue = nums[i];
+            int left = i + 1, right = numSize - 1;
+
+            while (left < right) {
+                auto cv = nums[left] + nums[right] + iValue;
+
+                if (cv < 0) {
+                    left += 1;
+                }else if (cv > 0) {
+                    right -= 1;
+                }else {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left += 1;
+                    
+                    while (nums[left] == nums[left - 1] && left < right) {
+                        left += 1;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
 
