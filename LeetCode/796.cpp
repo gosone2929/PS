4a8a64a0dbc61c0d043609de9a1eb71bf3cf9f7f
MaraMincho <seemaster300@naver.com>
2025-12-14 23:18:31 +0900

    ps

---
+++ LeetCode/796.cpp
@@ -0,0 +1,13 @@
+#include <iostream>>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        string ss = s + s;
+        return ss.find(goal) == string::npos ? false : true;
+    }
+};
